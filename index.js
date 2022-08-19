c3febd0952bc9304abfd5664fccd0fd856b4dd1c
ruthjelimo <ruthjmimo@gmail.com>
2022-08-19 10:57:53 +0300

    updated files

---
+++ index.js
@@ -0,0 +1,14 @@
+// function sendEmail() {
+// Email.send({
+// port:2525,
+// Host : 'smtp.elasticemail.com',
+// Username : 'ruthjmimo@gmail.com',
+// Password : '078079637CC175D45DF145BA5095760F70ED',
+// To : 'silvanussigei1996@gmail.com',
+// From : "ruthjmimo@gmail.com",
+// Subject : "Test email",
+// Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+// }).then(
+// message => alert(message)
+// );
+// }
